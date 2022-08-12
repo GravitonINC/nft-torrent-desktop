c0a46b13bc699c4b0a5af4613ab6895b27c1d32d
Braulio Lomeli <brauliolomeli@gmail.com>
2022-08-12 10:04:38 -0500

    WIP connect wallet UI

--- src/renderer/components/enter-otp-modal.js
+++ src/renderer/components/enter-otp-modal.js
@@ -1,16 +1,48 @@
 const React = require('react')
 const TextField = require('material-ui/TextField').default
+const Chip = require('material-ui/Chip').default
+const CustomButton = require('./custom-button')
+const GradientBorderButton = require('./gradient-border-button')
 const { clipboard } = require('electron')
 
 const ModalOKCancel = require('./modal-ok-cancel')
 const { dispatch, dispatcher } = require('../lib/dispatcher')
+const { shell } = require('electron')
+const config = require('../../config')
 
 module.exports = class EnterOtpModal extends React.Component {
-  render () {
+
+  openWebApp = () => {
+    shell.openExternal(config.GRAVITON_MAIN_WEB_APP_URL);
+  }
+
+  render() {
+    const contentStyle = {
+      display: 'grid',
+      gap: 8
+    }
     return (
       <div className='enter-otp-modal'>
-        <p><label>Enter OTP</label></p>
-        <div>
+        <div style={contentStyle}>
+          <Chip
+            backgroundColor="#443F55"
+            labelColor="#AAAAAA"
+          >
+            Step 1
+          </Chip>
+          <div>Generate your One Time Passcode in the Graviton NFT Torrent Web3 application.</div>
+          <GradientBorderButton
+            label='Open Web3 app'
+            fullWidth
+            onClick={this.openWebApp}
+          />
+          <Chip
+            backgroundColor="#443F55"
+            labelColor="#AAAAAA"
+          >
+            Step 2
+          </Chip>
+          <div>Enter your One Time Passcode below.</div>
           <TextField
             id='enter-otp-field'
             className='control'
@@ -29,7 +61,7 @@ module.exports = class EnterOtpModal extends React.Component {
     )
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.otp.input.focus()
     const clipboardContent = clipboard.readText()
 
@@ -40,11 +72,11 @@ module.exports = class EnterOtpModal extends React.Component {
   }
 }
 
-function handleKeyDown (e) {
+function handleKeyDown(e) {
   if (e.which === 13) handleOK.call(this) /* hit Enter to submit */
 }
 
-function handleOK () {
+function handleOK() {
   dispatch('exitModal')
   dispatch('exchangeOtp', this.otp.input.value)
 }
