67ba18d051c469bbec0e8da8470a39c844227bd5
Braulio Lomeli <brauliolomeli@gmail.com>
2022-08-12 15:36:56 -0500

    Wallet linked UI

--- src/renderer/components/wallet-connected.js
+++ src/renderer/components/wallet-connected.js
@@ -8,7 +8,24 @@ class WalletConnected extends React.Component {
   render() {
     const state = this.props.state.saved;
     const account = state && state.auth && state.auth.address;
-    return <span >Wallet {account}</span>;
+    const displayAccount = `${account.slice(0, 7)}...${account.slice(account.length - 4)}`;
+    return (
+      <>
+        <div className='space-between'>
+          <p>Wallet</p>
+          <a href=''>Unlink</a>
+        </div>
+        <div className='dark-box'>
+          <div className='space-between'>
+            <span>
+              {displayAccount}
+            </span>
+            <span>Linked</span>
+          </div>
+        </div>
+      </>
+
+    )
   }
 
 }
